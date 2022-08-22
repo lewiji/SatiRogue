9df141c8da22128ac31251e4d6099c2c0ee34613
lewiji <lewiji@gmail.com>
2022-08-22 16:18:45 +0100

    Add popupmenu to inventory items, handle item slots being empty

--- src/Ecs/Play/Nodes/Hud/ItemSlot.cs
+++ src/Ecs/Play/Nodes/Hud/ItemSlot.cs
@@ -1,42 +1,100 @@
+using System.Diagnostics.CodeAnalysis;
 using Godot;
 using GodotOnReady.Attributes;
 namespace SatiRogue.Ecs.Play.Nodes.Hud;
 
+[SuppressMessage("ReSharper", "FieldCanBeMadeReadOnly.Local")]
 [Tool]
 public partial class ItemSlot : CenterContainer {
    [Signal] public delegate void OnPressed(int itemIndex);
-   [OnReadyGet("TextureRect/CenterContainer")] private Control _clickTarget = null!;
 
-   private string _itemName = "";
+   private AtlasTexture _normalFrameTex = GD.Load<AtlasTexture>("res://src/Ecs/Play/Nodes/Hud/ItemFrameNormal.tres");
+   private AtlasTexture _focusFrameTex = GD.Load<AtlasTexture>("res://src/Ecs/Play/Nodes/Hud/ItemFrameSelected.tres");
+
+   [OnReadyGet("%ClickTarget")] private Control _clickTarget = null!;
+   [OnReadyGet("%Equipped")] private Control _equippedIndicator = null!;
+   [OnReadyGet("%ItemTexture")] private TextureRect _itemTextureRect = null!;
+   [OnReadyGet("%Frame")] private TextureRect _frameTextureRect = null!;
+   [OnReadyGet("%LabelName")] private Label _labelName = null!;
+   [OnReadyGet("%PopupMenu")] private PopupMenu _popupMenu = null!;
+
+   public bool IsEmpty {
+      get => _itemTexture == null;
+   }
+
+   private bool _isSelected = false;
+   public bool IsSelected {
+      get => !IsEmpty && _isSelected;
+      set {
+         if (IsEmpty) {
+            _isSelected = false;
+            _frameTextureRect.Texture = _normalFrameTex;
+         } else {
+            _isSelected = value;
+            _frameTextureRect.Texture = _isSelected ? _focusFrameTex : _normalFrameTex;
+         }
+         SetEmptyModulation();
+      }
+   }
+
    private Texture? _itemTexture;
-   [OnReadyGet] public Label? Label;
-   [OnReadyGet] public TextureRect? TextureRect;
    [Export] public Texture? ItemTexture {
       get => _itemTexture;
       set {
          _itemTexture = value;
-         if (TextureRect != null) TextureRect.Texture = _itemTexture;
+         _itemTextureRect.Texture = _itemTexture;
+         SetEmptyModulation();
       }
    }
+
+   private string _itemName = "";
    [Export] public string ItemName {
       get => _itemName;
       set {
          _itemName = value;
-         if (Label != null) Label.Text = _itemName;
+         _labelName.Text = _itemName;
       }
    }
+
+   private bool _equipped;
+   [Export] public bool Equipped {
+      get => _equipped;
+      set {
+         _equipped = value;
+         _equippedIndicator.Visible = _equipped;
+      }
+   }
+
    [Export] public string Description { get; set; } = "";
    [Export] public int Quantity { get; set; }
 
    [OnReady] private void SetInitial() {
-      if (TextureRect != null) TextureRect.Texture = _itemTexture;
-      if (Label != null) Label.Text = _itemName;
+      _itemTextureRect.Texture = _itemTexture;
+      _labelName.Text = _itemName;
       _clickTarget.Connect("gui_input", this, nameof(OnGuiInput));
+      _popupMenu.Connect("about_to_show", this, nameof(OnSelected));
+      _popupMenu.Connect("popup_hide", this, nameof(OnDeselected));
+      SetEmptyModulation();
+   }
+
+   private void SetEmptyModulation() {
+      _frameTextureRect.Modulate = IsEmpty ? new Color("7f9e9e9e") : new Color("ffffff");
+   }
+
+   private void OnSelected() {
+      IsSelected = true;
+   }
+
+   private void OnDeselected() {
+      IsSelected = false;
    }
 
    private void OnGuiInput(InputEvent @event) {
-      if (@event is InputEventMouseButton {Pressed: false} mouseJustPressed) {
-         EmitSignal(nameof(OnPressed), GetIndex());
-      }
+      if (@event is not InputEventMouseButton {Pressed: false} inputEventMouseButton) return;
+      if (IsEmpty) return;
+
+      EmitSignal(nameof(OnPressed), GetIndex());
+
+      _popupMenu.Popup_(new Rect2(inputEventMouseButton.GlobalPosition, new Vector2(55, 74)));
    }
 }
